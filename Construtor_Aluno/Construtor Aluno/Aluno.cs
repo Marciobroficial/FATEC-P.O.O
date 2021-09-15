d8d01c3c15b74729b667026bf031144c4abab5d9
Marcio Alessandro Dos Santos <marciobroficial@outlook.com>
2021-09-15 09:53:31 -0300

    Construtor

---
+++ Construtor_Aluno/Construtor Aluno/Aluno.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtor_Aluno
+{
+    class Aluno
+    {
+        private long ra;
+        private string nome;
+        private double idade;
+        private static long contador = 1570482121000;
+
+        public static long Contador
+        {
+            get
+            {
+                return contador;
+            }
+        }
+        public Aluno() 
+        {
+            contador = 1570482121001;
+        }
+        public Aluno(long ra)
+        {
+            this.ra = ra;
+            contador ++;
+        }
+        public Aluno(double idade)
+        {
+            this.idade = idade;
+            contador ++;
+        }
+        public Aluno(string nome)
+        {
+            this.nome = nome;
+            contador ++;
+        }
+        public Aluno(long ra, string nome, double idade)
+        {
+            this.ra = ra;
+            this.nome = nome;
+            this.idade = idade;
+            contador ++;
+        }
+        public void MostrarAtributos()
+        {
+            Console.WriteLine("RA: " + ra + "\tNome: " + nome + "\tIdade: " + idade + "");
+        }
+    }
+}
---
+++ Construtor_Aluno/Construtor Aluno/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtor_Aluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            Aluno[] vAluno = new Aluno[3];
+            for (int i = 0; i < vAluno.Length; i++)
+            {
+                Console.Write("\nInforme o nome do aluno: ");
+                string Nome = Console.ReadLine();
+                Console.Write("Informe a idade do aluno: ");
+                int Idade = Convert.ToInt32(Console.ReadLine());
+                long Ra = Aluno.Contador;
+                vAluno[i] = new Aluno(Ra, Nome, Idade);
+                Console.WriteLine("RA: " + Aluno.Contador);
+            }
+            Console.WriteLine(" ");
+            foreach (Aluno va in vAluno)
+                va.MostrarAtributos();
+            Console.WriteLine();
+            Console.WriteLine("Quantidade de instâncias? " + (Aluno.Contador - 1570482121000));
+            Console.ReadKey();
+        }
+    }
+}
